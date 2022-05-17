7b0491d59381340ef2e2c201538370a45e831d36
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-05-17 08:45:56 +0200

    add dati fake db

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Trains;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -9,7 +11,22 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        //
+        for ($i = 0; $i < 50; $i++) {
+            $newTran = new Trains();
+            $newTran->Agency = $faker->title();
+            $newTran->departure_station = $faker->city();
+            $newTran->arrival_station = $faker->city();
+            $newTran->departure_time = $faker->date();
+            $newTran->Arrival_time = $faker->date();
+            $newTran->train_code = $faker->randomNumber(1, 5);
+            $newTran->number_of_carriages = $faker->numberBetween(1, 20);
+            $newTran->in_time = $faker->boolean();
+            $newTran->deleted = $faker->boolean();
+            $newTran->price = $faker->numberBetween(1, 2000);
+            $newTran->save();
+        }
     }
 }
